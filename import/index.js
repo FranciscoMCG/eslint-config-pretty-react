af0a01657d72b29f1eac5444a7cb2c2701a64a17
Francisco Gomes <francisco.gomes1@sainsburys.co.uk>
2020-09-15 21:21:12 +0100

    added import but commented off. added to roadmap

--- import/index.js
+++ import/index.js
@@ -1,6 +1,77 @@
 module.exports = {
-  extends: ['../config.js'],
-  rules: {
-    // TODO eslint-import-rules
-  }
+  // eslint-plugin-import in the roadmap
+  // parser: '@typescript-eslint/parser',
+  // plugins: ['import'],
+  // env: {
+  //   es6: true
+  // },
+  // parserOptions: {
+  //   ecmaVersion: 2018,
+  //   sourceType: 'module'
+  // },
+  // settings: {
+  //   'import/ignore': ['node_modules', '.json$', '.(scss|less|css|styl)$']
+  // },
+  // extends: [
+  //   '../config.js',
+  //   'plugin:import/errors',
+  //   'plugin:import/warnings',
+  //   'plugin:import/typescript'
+  // ],
+  // rules: {
+  //   // Static analysis
+  //   'import/no-unresolved': 'error',
+  //   'import/named': 'error',
+  //   'import/default': 'error',
+  //   'import/namespace': 'error',
+  //   'import/no-restricted-paths': 'off',
+  //   'import/no-absolute-path': 'error',
+  //   'import/no-dynamic-require': 'off',
+  //   'import/no-internal-modules': 'off',
+  //   'import/no-webpack-loader-syntax': 'error',
+  //   'import/no-self-import': 'error',
+  //   'import/no-cycle': 'warn',
+  //   'import/no-useless-path-segments': 'off',
+  //   'import/no-relative-parent-imports': 'off',
+  //   // Helpful warnings
+  //   'import/export': 'error',
+  //   'import/no-named-as-default': 'error',
+  //   'import/no-named-as-default-member': 'error',
+  //   'import/no-deprecated': 'warn', // this is an in progress rule
+  //   'import/no-extraneous-dependencies': 'error',
+  //   'import/no-mutable-exports': 'error',
+  //   'import/no-unused-modules': 'off',
+  //   // Module systems
+  //   'import/unambiguous': 'off',
+  //   'import/no-commonjs': 'off',
+  //   'import/no-amd': 'error',
+  //   'import/no-nodejs-modules': 'off',
+  //   // Style guide
+  //   'import/first': 'error',
+  //   'import/exports-last': 'off',
+  //   'import/no-duplicates': 'error',
+  //   'import/no-namespace': 'off',
+  //   'import/extensions': 'error',
+  //   'import/order': [
+  //     'warn',
+  //     {
+  //       groups: [
+  //         'builtin',
+  //         ['external', 'internal'],
+  //         'parent',
+  //         ['sibling', 'index']
+  //       ]
+  //     }
+  //   ],
+  //   'import/newline-after-import': 'off',
+  //   'import/prefer-default-export': 'off',
+  //   'import/max-dependencies': 'off',
+  //   'import/no-unassigned-import': 'off',
+  //   'import/no-named-default': 'error',
+  //   'import/no-default-export': 'off',
+  //   'import/no-named-export': 'off',
+  //   'import/no-anonymous-default-export': 'off',
+  //   'import/group-exports': 'off',
+  //   'import/dynamic-import-chunkname': 'off'
+  // }
 };
